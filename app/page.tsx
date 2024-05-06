ca3378b067b0eb5eb337008ed2f6262807ec233f
Krish-Das <krishdasatgames@gmail.com>
2024-05-06 13:37:48 +0530

    Refactor: Route groups are now updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Main } from "@/components/layout/mainwrapper";
+
 export default function Home() {
   return (
-    <>
+    <Main>
       <h1>Hello Next js</h1>
-    </>
+    </Main>
   );
 }
