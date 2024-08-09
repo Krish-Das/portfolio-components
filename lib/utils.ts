889ad9a1f48fa9fba8148bdc3100bf94aca94ef3
Krish-Das <krishdasatgames@gmail.com>
2024-08-09 20:34:57 +0530

    Install: Shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function wait(milliseconds: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, milliseconds);
-  });
+  return twMerge(clsx(inputs))
 }
