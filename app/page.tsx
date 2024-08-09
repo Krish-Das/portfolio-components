039daf8e57fb38fef0f4ad0ffdbcd84d42e0be09
Krish-Das <krishdasatgames@gmail.com>
2024-08-09 20:44:30 +0530

    Install: Shancn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Main } from "@/components/layout/mainwrapper";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <Main>
       <h1>Hello Next js</h1>
+      <Button>Click me!</Button>
     </Main>
   );
 }
