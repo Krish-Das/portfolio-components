2f08f540b3654d6be6d7a5e8ff91995e682e7812
Krish-Das <krishdasatgames@gmail.com>
2024-08-09 21:46:23 +0530

    Add: Custom style on the pre-existing button

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,9 +1,29 @@
 import { Main } from "@/components/layout/mainwrapper";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { RiArrowRightDownLine } from "react-icons/ri";
 
 export default function ComponentPage() {
   return (
     <>
-      <Main>Compnent page</Main>
+      <Main>
+        <h2>Button</h2>
+
+        <HeroButton />
+      </Main>
     </>
   );
 }
+
+const HeroButton = ({ className }: { className?: string }) => {
+  return (
+    <Button
+      className={cn(
+        "h-9 rounded-full bg-foreground px-5 font-[400] leading-none text-background",
+        className,
+      )}
+    >
+      Enter <RiArrowRightDownLine />
+    </Button>
+  );
+};
