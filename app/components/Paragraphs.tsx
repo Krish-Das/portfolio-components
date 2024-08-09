1e14f7971242985acc2f74a43cb431f9acba4203
Krish-Das <krishdasatgames@gmail.com>
2024-08-09 22:39:46 +0530

    Style: Increase font size for readable-paragraphs

--- app/components/Paragraphs.tsx
+++ app/components/Paragraphs.tsx
@@ -2,13 +2,10 @@ import { cn } from "@/lib/utils";
 
 const ParagraphDefault = () => {
   return (
-    <>
-      <Paragraph />
-      <br />
-
+    <div className="flex w-full items-center justify-center">
       <Paragraph preferReadability />
       <br />
-    </>
+    </div>
   );
 };
 
@@ -25,14 +22,38 @@ const Paragraph = ({
         className={cn(
           "tracking-[0.02em]",
           limitWidt && "max-w-[50ch]",
-          preferReadability && "max-w-[50ch]",
+          preferReadability && "max-w-[50ch] text-lg",
         )}
       >
         A cutting-edge real-time collaboration app designed to supercharge
-        productivity.{"\n"}In publishing and graphic design, Lorem ipsum is a
-        placeholder text commonly used to demonstrate the visual form of a
-        document or a typeface without relying on meaningful content. Lorem
-        ipsum may be used as a placeholder before the final copy is available.
+        productivity.
+        <br />
+        In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before the final copy is available.
+        <br />
+        <br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the {"industry's"} standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <br />
+        <br />
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using {"Content here, content here"}, making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for {"lorem ipsum"} will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
       </p>
     </>
   );
