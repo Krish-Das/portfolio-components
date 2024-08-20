cbc77576e966dc467646102bb51d231589521d2d
Krish-Das <krishdasatgames@gmail.com>
2024-08-20 20:32:18 +0530

    Style: Increase button font-size for mobile

--- app/components/Buttons.tsx
+++ app/components/Buttons.tsx
@@ -11,9 +11,9 @@ const ButtonDefault = ({
   return (
     <Button
       className={cn(
-        "inline-flex items-center justify-center gap-1 rounded-full bg-foreground px-5 font-[400] leading-none text-background",
-        "h-12 min-w-12", // Base size
-        "sm:h-9 sm:min-h-9", // tablet/desktop
+        "inline-flex items-center justify-center gap-1 rounded-full bg-foreground px-5 leading-none tracking-[0.01em] text-background",
+        "h-12 min-w-12 text-base", // Base size
+        "sm:h-9 sm:min-h-9 sm:text-sm", // tablet/desktop
         className,
       )}
     >
@@ -32,9 +32,9 @@ const ButtonSmall = ({
   return (
     <Button
       className={cn(
-        "inline-flex items-center justify-center gap-1 rounded-full bg-foreground px-4 font-[400] leading-none text-background",
-        "h-10 min-w-10", // Base size
-        "sm:h-8 sm:min-h-8", // tablet/desktop
+        "inline-flex items-center justify-center gap-1 rounded-full bg-foreground px-4 text-xs leading-none tracking-[0.01em] text-background",
+        "h-10 min-w-10 text-sm", // Base size
+        "sm:h-8 sm:min-h-8 sm:text-xs", // tablet/desktop
         className,
       )}
     >
