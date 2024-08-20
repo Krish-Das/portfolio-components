bf4feb081a446f6ecf24b655f4fd60691c45eb4f
Krish-Das <krishdasatgames@gmail.com>
2024-08-20 18:17:57 +0530

    Add: Buttons on the page

--- app/next-ui/page.tsx
+++ app/next-ui/page.tsx
@@ -1,11 +1,24 @@
 import { Main } from "@/components/layout/mainwrapper";
 import { Button } from "@nextui-org/button";
+import { IoMenu, IoOpen } from "react-icons/io5";
 
 export default function Page() {
   return (
     <>
-      <Main>
-        <Button>Click me</Button>
+      <Main className="relative grid min-h-[100dvh] place-content-center">
+        <Button startContent=<IoOpen /> color="primary">
+          Enter
+        </Button>
+
+        <Button
+          color="primary"
+          variant="flat"
+          className="absolute right-0 top-0 -translate-x-4 translate-y-4"
+          size="lg"
+          isIconOnly
+        >
+          <IoMenu />
+        </Button>
       </Main>
     </>
   );
