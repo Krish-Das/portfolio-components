5ef997d26c71029998ef5ae8e58a0c7dbbdbcbc4
Krish-Das <krishdasatgames@gmail.com>
2024-08-21 21:49:39 +0530

    Add: Animation when button is ckicked (complete)

--- app/components/RacRadixButton.tsx
+++ app/components/RacRadixButton.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { ButtonHTMLAttributes, useRef } from "react";
 import { cn } from "@/lib/utils";
 import { useButton } from "@react-aria/button";
 import { FocusRing } from "@react-aria/focus";
-import { useRef } from "react";
+import { MotionProps, motion, useAnimation } from "framer-motion";
 
 const RacRadixButton = ({
   children,
@@ -15,26 +16,40 @@ const RacRadixButton = ({
   onClick?: () => void;
 }) => {
   const ref = useRef<HTMLButtonElement | null>(null);
-  const { buttonProps, isPressed } = useButton({ onPress: onClick }, ref);
-
-  //
+  const { buttonProps } = useButton(
+    {
+      onPressStart: () => {
+        controls.stop();
+        controls.set({ background: "#757376" });
+      },
+      onPressEnd: () => {
+        controls.start({ background: ["#757376", "#353336"] });
+      },
+      onPress: onClick,
+    },
+    ref,
+  );
+  const controls = useAnimation();
 
   return (
     <FocusRing focusRingClass="outline-none ring ring-offset-2 ring-offset-background">
-      <button
+      {/* TODO: optimize this button prop types */}
+      <motion.button
+        style={{ WebkitTapHighlightColor: "transparent" }}
         className={cn(
-          "bg-[#424245]s inline-flex h-20 w-20 items-center justify-center gap-2 rounded-full bg-[#353336] text-xl text-foreground",
+          "inline-flex h-20 w-20 items-center justify-center gap-2 rounded-full bg-[#353336] text-xl text-foreground",
           "focus:outline-none",
-          isPressed && "bg-[#757376]",
           "touch-none select-none",
           className,
         )}
+        animate={controls}
         onClick={onClick}
-        {...buttonProps}
+        {...(buttonProps as ButtonHTMLAttributes<HTMLButtonElement> &
+          MotionProps)}
         ref={ref}
       >
         {children}
-      </button>
+      </motion.button>
     </FocusRing>
   );
 };
