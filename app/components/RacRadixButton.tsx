93ab29d8d167e02ba0cb64c18866136d93fdd3a7
Krish-Das <krishdasatgames@gmail.com>
2024-08-21 21:12:11 +0530

    Add:  to the button

--- app/components/RacRadixButton.tsx
+++ app/components/RacRadixButton.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { useButton } from "@react-aria/button";
+import { FocusRing } from "@react-aria/focus";
 import { useRef } from "react";
 
 const RacRadixButton = ({
@@ -16,21 +17,25 @@ const RacRadixButton = ({
   const ref = useRef<HTMLButtonElement | null>(null);
   const { buttonProps, isPressed } = useButton({ onPress: onClick }, ref);
 
+  //
+
   return (
-    <button
-      className={cn(
-        "bg-[#424245]s inline-flex h-20 w-20 items-center justify-center gap-2 rounded-full bg-[#353336] text-xl text-foreground",
-        "focus:outline-none focus-visible:ring focus-visible:ring-offset-2 focus-visible:ring-offset-background ",
-        isPressed && "bg-[#757376]",
-        "touch-none select-none",
-        className,
-      )}
-      onClick={onClick}
-      {...buttonProps}
-      ref={ref}
-    >
-      {children}
-    </button>
+    <FocusRing focusRingClass="outline-none ring ring-offset-2 ring-offset-background">
+      <button
+        className={cn(
+          "bg-[#424245]s inline-flex h-20 w-20 items-center justify-center gap-2 rounded-full bg-[#353336] text-xl text-foreground",
+          "focus:outline-none",
+          isPressed && "bg-[#757376]",
+          "touch-none select-none",
+          className,
+        )}
+        onClick={onClick}
+        {...buttonProps}
+        ref={ref}
+      >
+        {children}
+      </button>
+    </FocusRing>
   );
 };
 
