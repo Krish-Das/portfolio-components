576221a59a32155c59df7f1928e99f418e5af7d8
Krish-Das <krishdasatgames@gmail.com>
2024-08-25 21:02:09 +0530

    Add: random number on click

--- app/components/ButtonLab.tsx
+++ app/components/ButtonLab.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { RacRadixButton } from "./RacRadixButton";
+import { IoLogoGoogle } from "react-icons/io";
 import { HiOutlineDownload } from "react-icons/hi";
 import { Button as TestButton } from "./TestButton";
 
@@ -12,13 +13,16 @@ export default function ButtonLab() {
     <>
       <Display number={number} />
 
-      <div className="mt-5 inline-flex h-[70dvh] w-full items-center justify-center">
+      <div className="mt-5 inline-flex h-[70dvh] w-full items-center justify-center gap-2">
         <RacRadixButton
           onClick={() => setNumber(Math.round(Math.random() * 100))}
         >
           <HiOutlineDownload /> Download
         </RacRadixButton>
-        <TestButton>this</TestButton>
+
+        <TestButton onClick={() => setNumber(Math.round(Math.random() * 100))}>
+          <IoLogoGoogle /> Google
+        </TestButton>
       </div>
     </>
   );
