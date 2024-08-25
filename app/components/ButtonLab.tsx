932252a7b1fe885b489a6425134b975cfd16e512
Krish-Das <krishdasatgames@gmail.com>
2024-08-25 20:34:00 +0530

    Style: Add animation and basic styles to button component

--- app/components/ButtonLab.tsx
+++ app/components/ButtonLab.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { RacRadixButton } from "./RacRadixButton";
 import { HiOutlineDownload } from "react-icons/hi";
+import { Button as TestButton } from "./TestButton";
 
 export default function ButtonLab() {
   const [number, setNumber] = useState(0);
@@ -17,6 +18,7 @@ export default function ButtonLab() {
         >
           <HiOutlineDownload /> Download
         </RacRadixButton>
+        <TestButton>this</TestButton>
       </div>
     </>
   );
--- app/components/TestButton.tsx
+++ app/components/TestButton.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, { ButtonHTMLAttributes, forwardRef, useRef } from "react";
+
+import { cn } from "@/lib/utils";
+import { FocusRing } from "@react-aria/focus";
 import { useButton, AriaButtonProps } from "@react-aria/button";
 import { MotionProps, motion, useAnimation } from "framer-motion";
 
@@ -31,15 +34,24 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     );
 
     return (
-      <motion.button
-        {...(buttonProps as ButtonHTMLAttributes<HTMLButtonElement> &
-          MotionProps)}
-        animate={controls}
-        className={className}
-        ref={localRef}
-      >
-        {children}
-      </motion.button>
+      <FocusRing focusRingClass="outline-none ring ring-offset-2 ring-offset-background">
+        <motion.button
+          {...(buttonProps as ButtonHTMLAttributes<HTMLButtonElement> &
+            MotionProps)}
+          animate={controls}
+          className={cn(
+            "inline-flex items-center justify-center gap-1 rounded-full bg-[#353336] px-5 leading-none tracking-[0.01em] text-foreground",
+            "focus:outline-none",
+            "touch-none select-none", // Disable select
+            "h-12 min-w-12 text-base", // Base size
+            "sm:h-9 sm:min-h-9 sm:text-sm", // tablet/desktop
+            className,
+          )}
+          ref={localRef}
+        >
+          {children}
+        </motion.button>
+      </FocusRing>
     );
   },
 );
