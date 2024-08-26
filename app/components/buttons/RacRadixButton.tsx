a092d1d58003c07ae21996aa7cd28e4fe76f99a7
Krish-Das <krishdasatgames@gmail.com>
2024-08-26 12:02:39 +0530

    Add: Props that accept all button props for RacRadixButton

--- app/components/buttons/RacRadixButton.tsx
+++ app/components/buttons/RacRadixButton.tsx
@@ -1,59 +1,60 @@
 "use client";
 
-import { ButtonHTMLAttributes, forwardRef, useRef } from "react";
+import React, { ButtonHTMLAttributes, forwardRef, useRef } from "react";
+
 import { cn } from "@/lib/utils";
-import { useButton } from "@react-aria/button";
 import { FocusRing } from "@react-aria/focus";
+import { useButton, AriaButtonProps } from "@react-aria/button";
 import { MotionProps, motion, useAnimation } from "framer-motion";
 
-const RacRadixButton = forwardRef<
-  HTMLButtonElement,
-  {
-    children: React.ReactNode;
-    className?: string;
-    onClick?: () => void;
-  }
->(({ children, className, onClick }, ref) => {
-  const localRef = useRef<HTMLButtonElement | null>(null);
-  const { buttonProps } = useButton(
-    {
-      onPressStart: () => {
-        controls.stop();
-        controls.set({ background: "#757376" });
-      },
-      onPressEnd: () => {
-        controls.start({ background: "#353336" });
-      },
-      onPress: onClick,
-    },
-    localRef,
-  );
-  const controls = useAnimation();
+type ButtonProps = AriaButtonProps & {
+  className?: string;
+  onClick?: () => void;
+};
 
-  return (
-    <FocusRing focusRingClass="outline-none ring ring-offset-2 ring-offset-background">
-      {/* TODO: optimize this button prop types */}
-      <motion.button
-        style={{ WebkitTapHighlightColor: "transparent" }}
-        className={cn(
-          "inline-flex items-center justify-center gap-1 rounded-full bg-[#353336] px-5 leading-none tracking-[0.01em] text-foreground",
-          "focus:outline-none",
-          "touch-none select-none", // Disable select
-          "h-12 min-w-12 text-base", // Base size
-          "sm:h-9 sm:min-h-9 sm:text-sm", // tablet/desktop
-          className,
-        )}
-        animate={controls}
-        onClick={onClick}
-        {...(buttonProps as ButtonHTMLAttributes<HTMLButtonElement> &
-          MotionProps)}
-        ref={ref}
-      >
-        {children}
-      </motion.button>
-    </FocusRing>
-  );
-});
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className, onClick, ...props }, ref) => {
+    const domRef = useRef<HTMLButtonElement>(null);
+    const localRef = ref || domRef;
+    const controls = useAnimation();
 
-RacRadixButton.displayName = "button";
-export { RacRadixButton };
+    const { buttonProps } = useButton(
+      {
+        onPressStart: () => {
+          controls.stop();
+          controls.set({ background: "#757376" });
+        },
+        onPressEnd: () => {
+          controls.start({ background: "#353336" });
+        },
+        onPress: onClick,
+        ...props,
+      },
+      localRef as React.RefObject<HTMLButtonElement>,
+    );
+
+    return (
+      <FocusRing focusRingClass="outline-none ring ring-offset-2 ring-offset-background">
+        <motion.button
+          {...(buttonProps as ButtonHTMLAttributes<HTMLButtonElement> &
+            MotionProps)}
+          animate={controls}
+          className={cn(
+            "inline-flex items-center justify-center gap-1 rounded-full bg-[#353336] px-5 leading-none tracking-[0.01em] text-foreground",
+            "focus:outline-none",
+            "touch-none select-none", // Disable select
+            "h-12 min-w-12 text-base", // Base size
+            "sm:h-9 sm:min-h-9 sm:text-sm", // tablet/desktop
+            className,
+          )}
+          ref={localRef}
+        >
+          {children}
+        </motion.button>
+      </FocusRing>
+    );
+  },
+);
+
+Button.displayName = "Button";
+export { Button as RacRadixButton };
