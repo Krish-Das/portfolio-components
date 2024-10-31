7bea6b1ee5da9e2831c0e7421e55128f58d4dbee
Krish-Das <krishdasatgames@gmail.com>
2024-10-31 12:10:11 +0530

    Add: sample project images

---
+++ lib/project-images.ts
@@ -0,0 +1,31 @@
+export const heroImage =
+  "https://assets.awwwards.com/awards/images/2024/06/metalab-cs-cover.jpg";
+
+export const projects = [
+  {
+    id: "04ah3",
+    label: "Arcadia",
+    imageLink:
+      // "https://mir-s3-cdn-cf.behance.net/project_modules/1400/fe0e1d176922383.64cd13eb4676e.jpg",
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/975f0b204481991.66a9c401c0232.jpg",
+    tags: ["Brand design", "Developement"],
+    completed: true,
+  },
+  {
+    id: "23a5J",
+    label: "Hyundai",
+    imageLink:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/078371132230211.61de2a994cb5a.jpg",
+    tags: ["Brand design", "Developement", "Intaraction design"],
+    completed: true,
+  },
+  {
+    id: "c92ho",
+    label: "New project",
+    imageLink: "",
+    tags: [],
+    completed: false,
+  },
+];
+
+export type ProjectDataType = typeof projects;
