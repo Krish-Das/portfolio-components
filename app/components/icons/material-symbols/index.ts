9fabaf45697a2803f2e812586f3a06588aa48ef2
Krish-Das <krishdasatgames@gmail.com>
2024-10-31 12:09:47 +0530

    Add: new utils functions and material icons

---
+++ app/components/icons/material-symbols/index.ts
@@ -0,0 +1,29 @@
+import { MaterialSymbolsArrowOutward } from "./MaterialSymbolsArrowOutward";
+import { MaterialSymbolsBroadcastOnPersonalRounded } from "./MaterialSymbolsBroadcastOnPersonalRounded";
+import { MaterialSymbolsCasesOutline } from "./MaterialSymbolsCasesOutline";
+import { MaterialSymbolsDockToLeft } from "./MaterialSymbolsDockToLeft";
+import { MaterialSymbolsDockToRight } from "./MaterialSymbolsDockToRight";
+import { MaterialSymbolsInfoOutline } from "./MaterialSymbolsInfoOutline";
+import { MaterialSymbolsMagnificationLarge } from "./MaterialSymbolsMagnificationLarge";
+import { MaterialSymbolsMail } from "./MaterialSymbolsMail";
+import { MaterialSymbolsMailOutline } from "./MaterialSymbolsMailOutline";
+import { MaterialSymbolsMenu } from "./MaterialSymbolsMenu";
+import { MaterialSymbolsMenuOpen } from "./MaterialSymbolsMenuOpen";
+import { MaterialSymbolsRectangleRounded } from "./MaterialSymbolsRectangleRounded";
+import { MaterialSymbolsWork } from "./MaterialSymbolsWork";
+
+export {
+  MaterialSymbolsDockToLeft,
+  MaterialSymbolsDockToRight,
+  MaterialSymbolsMenuOpen,
+  MaterialSymbolsMenu,
+  MaterialSymbolsArrowOutward,
+  MaterialSymbolsMailOutline,
+  MaterialSymbolsMail,
+  MaterialSymbolsMagnificationLarge,
+  MaterialSymbolsRectangleRounded,
+  MaterialSymbolsCasesOutline,
+  MaterialSymbolsInfoOutline,
+  MaterialSymbolsWork,
+  MaterialSymbolsBroadcastOnPersonalRounded,
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// eslint-disable-next-line no-unused-vars
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): T & { cancel: () => void } {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  const debounced = (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
+
+  debounced.cancel = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  };
+
+  return debounced as T & { cancel: () => void };
 }
