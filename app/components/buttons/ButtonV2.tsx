ba9f7f8405af694b964cfdfa059013b1abc75f6a
Krish-Das <krishdasatgames@gmail.com>
2024-10-31 15:55:31 +0530

    Add: while-tap animation

--- app/components/buttons/ButtonV2.tsx
+++ app/components/buttons/ButtonV2.tsx
@@ -9,9 +9,10 @@ import {
   ButtonProps as RacButtonProps,
 } from "react-aria-components";
 import { motion, MotionProps } from "framer-motion";
+import { cva, VariantProps } from "class-variance-authority";
 
-// TODO: Rename
-type NewButtonProps = Omit<RacButtonProps, "onPress"> &
+type ButtonProps = VariantProps<typeof buttonVariants> &
+  Omit<RacButtonProps, "onPress"> &
   MotionProps &
   (
     | (RacButtonProps & {
@@ -26,52 +27,63 @@ type NewButtonProps = Omit<RacButtonProps, "onPress"> &
       })
   );
 
-// TODO: Rename
-const FButton = forwardRef<HTMLButtonElement, RacButtonProps>((props, ref) => (
-  <RacButton {...props} ref={ref} />
-));
-FButton.displayName = "FButton";
-const MotionButton = motion.create(FButton);
-
-// TODO: Rename
-const NewButton = forwardRef<HTMLButtonElement, NewButtonProps>(
-  ({ className, onPress, onClick, ...props }, ref) => {
-    function handleClick(e: RacPressEvent) {
-      if (onClick) onClick(e);
-      if (onPress) onPress(e);
-    }
-    return (
-      <MotionButton
-        className={cn("p-2", className)}
-        onPress={handleClick}
-        {...props}
-        ref={ref}
-      />
-    );
+const buttonVariants = cva(
+  [
+    "inline-flex gap-2 items-center justify-center rounded-full px-5 leading-none tracking-[0.01em] whitespace-nowrap",
+    "focus:outline-none focus-visible:outline-none",
+    "disabled:pointer-events-none disabled:opacity-50", // Button Disabled
+    "touch-none cursor-default select-none", // Disable select
+    "[--bg-while-tap:#757376]",
+  ],
+  {
+    variants: {
+      variant: {
+        default: "bg-[#353336] text-foreground",
+        destructive: "bg-[#FF453A] text-foreground",
+      },
+      size: {
+        default: "h-12 sm:h-9 text-base sm:text-sm",
+        sm: "gap-1 h-10 sm:h-8 px-4 text-sm",
+        lg: "h-14 sm:h-11 text-base sm:text-sm",
+        icon: "h-14 w-14 p-0 sm:h-9 sm:w-9 text-xl sm:text-base",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
   },
 );
-NewButton.displayName = "NewButton";
 
-/*
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, onPress, onClick, ...props }, ref) => {
+  ({ className, onPress, onClick, variant, size, ...props }, ref) => {
     function handleClick(e: RacPressEvent) {
       if (onClick) onClick(e);
       if (onPress) onPress(e);
     }
 
     return (
-      <RacButton
-        className={cn("p-2", className)}
-        ref={ref}
+      <MotionButton
+        className={cn(buttonVariants({ variant, size, className }))}
+        whileTap={{ background: "var(--bg-while-tap)" }}
         onPress={handleClick}
         {...props}
+        ref={ref}
       />
     );
   },
 );
 Button.displayName = "Button";
-*/
 
-// TODO: Export the prop
-export { NewButton as Button };
+/**
+ *
+ * Motion and Forwarded buttons
+ *
+ **/
+const ForwardedButton = forwardRef<HTMLButtonElement, RacButtonProps>(
+  (props, ref) => <RacButton {...props} ref={ref} />,
+);
+ForwardedButton.displayName = "ForwardedButton";
+const MotionButton = motion.create(ForwardedButton);
+
+export { Button, ForwardedButton, type ButtonProps };
