5b0a57a02bbe4d159de7b9b82d6d851836bb2b93
Krish-Das <krishdasatgames@gmail.com>
2024-10-31 19:27:43 +0530

    Fix: height of the child component

--- app/ios-menu/page.tsx
+++ app/ios-menu/page.tsx
@@ -16,13 +16,13 @@ export default function Page() {
  */
 function Children() {
   return (
-    <main className="h-[100dvh] w-full transition-all">
-      <section className="grid h-full w-full place-items-center bg-[url('https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/4edaef206261021.66f7b8796624f.png')] bg-cover bg-center">
+    <main className="w-full transition-all">
+      <section className="grid h-[100dvh] w-full place-items-center bg-[url('https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/4edaef206261021.66f7b8796624f.png')] bg-cover bg-center">
         <Button>
           <SystemUiconsDisplayAlt /> Enter
         </Button>
       </section>
-      <section className="grid h-full w-full place-items-center bg-[url('https://assets.awwwards.com/awards/images/2024/06/metalab-cs-cover.jpg')] bg-cover bg-center" />
+      <section className="grid h-[100dvh] w-full place-items-center bg-[url('https://assets.awwwards.com/awards/images/2024/06/metalab-cs-cover.jpg')] bg-cover bg-center" />
     </main>
   );
 }
