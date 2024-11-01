40908bb758daa52b99a98148d7a259afbd6d1db8
Krish-Das <krishdasatgames@gmail.com>
2024-11-01 20:22:12 +0530

    Add: `dvh` height in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      spacing: {
+        dvh: "100dvh",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
