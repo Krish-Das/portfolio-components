d095bc7dda01cd871a1e24f08c58b5f867bda912
Krish-Das <krishdasatgames@gmail.com>
2024-11-01 16:36:03 +0530

    Add: new material symbols

--- app/components/icons/material-symbols/index.ts
+++ app/components/icons/material-symbols/index.ts
@@ -1,16 +1,20 @@
 import { MaterialSymbolsArrowOutward } from "./MaterialSymbolsArrowOutward";
 import { MaterialSymbolsBroadcastOnPersonalRounded } from "./MaterialSymbolsBroadcastOnPersonalRounded";
+import { MaterialSymbolsCallOutline } from "./MaterialSymbolsCallOutline";
 import { MaterialSymbolsCasesOutline } from "./MaterialSymbolsCasesOutline";
 import { MaterialSymbolsDockToLeft } from "./MaterialSymbolsDockToLeft";
 import { MaterialSymbolsDockToRight } from "./MaterialSymbolsDockToRight";
+import { MaterialSymbolsGroupOutline } from "./MaterialSymbolsGroupOutline";
 import { MaterialSymbolsInfoOutline } from "./MaterialSymbolsInfoOutline";
 import { MaterialSymbolsMagnificationLarge } from "./MaterialSymbolsMagnificationLarge";
 import { MaterialSymbolsMail } from "./MaterialSymbolsMail";
 import { MaterialSymbolsMailOutline } from "./MaterialSymbolsMailOutline";
 import { MaterialSymbolsMenu } from "./MaterialSymbolsMenu";
 import { MaterialSymbolsMenuOpen } from "./MaterialSymbolsMenuOpen";
+import { MaterialSymbolsNewspaper } from "./MaterialSymbolsNewspaper";
 import { MaterialSymbolsRectangleRounded } from "./MaterialSymbolsRectangleRounded";
 import { MaterialSymbolsWork } from "./MaterialSymbolsWork";
+import { MaterialSymbolsWorkOutlineSharp } from "./MaterialSymbolsWorkOutlineSharp";
 
 export {
   MaterialSymbolsDockToLeft,
@@ -26,4 +30,8 @@ export {
   MaterialSymbolsInfoOutline,
   MaterialSymbolsWork,
   MaterialSymbolsBroadcastOnPersonalRounded,
+  MaterialSymbolsGroupOutline,
+  MaterialSymbolsNewspaper,
+  MaterialSymbolsCallOutline,
+  MaterialSymbolsWorkOutlineSharp,
 };
