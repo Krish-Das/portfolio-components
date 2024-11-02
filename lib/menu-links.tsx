bb998764bfa9bc44960d25a12f6783125de41745
Krish-Das <krishdasatgames@gmail.com>
2024-11-02 14:22:00 +0530

    Add: menu-links and make background-color full black

---
+++ lib/menu-links.tsx
@@ -0,0 +1,11 @@
+import { MaterialSymbolsCallOutline } from "@/app/components/icons/material-symbols/MaterialSymbolsCallOutline";
+import { MaterialSymbolsGroupOutline } from "@/app/components/icons/material-symbols/MaterialSymbolsGroupOutline";
+import { MaterialSymbolsNewspaper } from "@/app/components/icons/material-symbols/MaterialSymbolsNewspaper";
+import { MaterialSymbolsWorkOutlineSharp } from "@/app/components/icons/material-symbols/MaterialSymbolsWorkOutlineSharp";
+
+export const MenuLinks = [
+  { label: "Works", icon: <MaterialSymbolsWorkOutlineSharp /> },
+  { label: "About us", icon: <MaterialSymbolsGroupOutline /> },
+  { label: "Latest news", icon: <MaterialSymbolsNewspaper /> },
+  { label: "Get in touch", icon: <MaterialSymbolsCallOutline /> },
+];
