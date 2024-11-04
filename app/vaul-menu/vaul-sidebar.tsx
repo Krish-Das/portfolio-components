dea4bd998ea52dcbfe916bb142e3eb9c8e835f13
Krish-Das <krishdasatgames@gmail.com>
2024-11-04 22:01:36 +0530

    Refactor: file-structure

--- app/vaul-menu/vaul-sidebar.tsx
+++ app/vaul-menu/vaul-sidebar.tsx
@@ -2,14 +2,8 @@
 
 import { Drawer } from "vaul";
 import { Button } from "@/app/components/buttons/ButtonV2";
-import {
-  MaterialSymbolsArrowOutward,
-  MaterialSymbolsMail,
-  MaterialSymbolsRectangleRounded,
-} from "@/app/components/icons/material-symbols";
-import { MenuLinks } from "@/lib/menu-links";
-import { cn } from "@/lib/utils";
-import { IoChevronForwardSharp } from "react-icons/io5";
+import { MaterialSymbolsRectangleRounded } from "@/app/components/icons/material-symbols";
+import MenuRight from "./menu-right";
 
 export default function VaulSidebar() {
   return (
@@ -44,43 +38,5 @@ export default function VaulSidebar() {
         </Drawer.Portal>
       </Drawer.Root>
     </>
-  );
-}
-
-function MenuRight() {
-  return (
-    <ul className="flex h-full w-72 flex-col gap-2 rounded-md md:gap-1">
-      {MenuLinks.map((link, idx) => (
-        <li key={idx} className="">
-          {/* TODO: Use variant ghost when added */}
-          <Button
-            className={cn(
-              "[&>svg:first-of-type]:text-foreground/85",
-              "bg-[hsl(0,0%,0%,0.0)] [--bg-tap-end:hsla(0,0%,0%,0.0)]",
-              "rac-hover:[--bg-tap-end:hsla(0,0%,100%,0.1)]",
-            )}
-          >
-            {link.icon}
-            {link.label}
-          </Button>
-        </li>
-      ))}
-      <li className="mt-10 flex h-full flex-col gap-3 px-4">
-        <p className="flex items-center gap-2 py-1 font-bold tracking-wide text-muted-foreground sm:text-sm">
-          <IoChevronForwardSharp />
-          Case studies
-        </p>
-        <div className="flex-1" />
-        <div className="inline-flex items-center gap-1">
-          <Button className="[&>svg:first-of-type]:text-foreground/75">
-            <MaterialSymbolsArrowOutward />
-            Open project
-          </Button>
-          <Button size="icon">
-            <MaterialSymbolsMail />
-          </Button>
-        </div>
-      </li>
-    </ul>
   );
 }
