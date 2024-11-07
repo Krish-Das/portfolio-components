b0193491f64d6bacc9613d7b043f7d41d6140697
Krish-Das <krishdasatgames@gmail.com>
2024-11-07 16:55:41 +0530

    Add new system-ui icons

--- app/components/icons/system-ui/index.ts
+++ app/components/icons/system-ui/index.ts
@@ -1,3 +1,4 @@
 import { SystemUiconsDisplayAlt } from "./SystemUiconsDisplayAlt";
+import { SystemUiconsEpisodes } from "./SystemUiconsEpisodes";
 
-export { SystemUiconsDisplayAlt };
+export { SystemUiconsDisplayAlt, SystemUiconsEpisodes };
