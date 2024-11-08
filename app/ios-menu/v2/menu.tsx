d2d18da02c3e343b48fbd756b836b1f669e8cac4
Krish-Das <krishdasatgames@gmail.com>
2024-11-08 21:17:43 +0530

    Add menu close delay only when the menu is open

--- app/ios-menu/v2/menu.tsx
+++ app/ios-menu/v2/menu.tsx
@@ -6,7 +6,7 @@ import {
   MaterialSymbolsRectangleRounded,
 } from "@/app/components/icons/material-symbols";
 import { transition } from "@/lib/animation-utils";
-import { cn } from "@/lib/utils";
+import { cn, wait } from "@/lib/utils";
 import * as Dialog from "@radix-ui/react-dialog";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
@@ -14,6 +14,8 @@ import useMeasure from "react-use-measure";
 
 export default function Menu() {
   const [open, setOpen] = useState(false);
+  const [shouldOverlayExitHaveDelay, setShouldOverlayExitHaveDelay] =
+    useState(false);
   const [ref, bounds] = useMeasure();
   const overlayCloseDelay = 0.5;
 
@@ -22,6 +24,11 @@ export default function Menu() {
       <Dialog.Root open={open} onOpenChange={setOpen}>
         <Dialog.Trigger
           autoFocus
+          onClick={async () => {
+            setShouldOverlayExitHaveDelay(false);
+            await wait(500);
+            setShouldOverlayExitHaveDelay(true);
+          }}
           ref={ref}
           className={cn(
             buttonVariants({ size: "icon", className: "bg-background/0" }),
@@ -56,7 +63,12 @@ export default function Menu() {
                       left: bounds.left,
                       bottom: window.innerHeight - bounds.bottom,
                       right: window.innerWidth - bounds.right,
-                      transition: { ...transition, delay: overlayCloseDelay },
+                      transition: {
+                        ...transition,
+                        delay: shouldOverlayExitHaveDelay
+                          ? overlayCloseDelay
+                          : 0,
+                      },
                     },
                     open: {
                       borderRadius: "0px",
@@ -78,7 +90,7 @@ export default function Menu() {
               >
                 <motion.div
                   className={cn(
-                    "w-[90vw] sm:w-[28rem] h-[86dvh] rounded-sm bg-[#3f3f46] origin-top-right",
+                    "menu__thumbnail w-[90vw] sm:w-[28rem] h-[86dvh] rounded-sm bg-[#3f3f46] origin-top-right",
                     "bg-cover bg-center bg-[url('https://dr.savee-cdn.com/image-fallbacks/original/6/5/6506b84c19486e146dac5b.jpg')]",
                     "[--width-to:90vw] sm:[--width-to:28rem]",
                     // 'https://dr.savee-cdn.com/image-fallbacks/original/6/7/26d96e3c9caf177be4e9d4.jpg',
