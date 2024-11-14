644cd9ccccc351a35ca176ea49f173bc6c3d79fe
Krish-Das <krishdasatgames@gmail.com>
2024-11-15 00:32:15 +0530

    Add new utility function `callWithDelay`

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,37 +1,46 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function wait(milliseconds: number) {
   return new Promise((resolve) => {
-    setTimeout(resolve, milliseconds);
-  });
+    setTimeout(resolve, milliseconds)
+  })
 }
 
 // eslint-disable-next-line no-unused-vars
 export function debounce<T extends (...args: any[]) => any>(
   func: T,
-  wait: number,
+  wait: number
 ): T & { cancel: () => void } {
-  let timeout: ReturnType<typeof setTimeout> | null = null;
+  let timeout: ReturnType<typeof setTimeout> | null = null
 
   const debounced = (...args: Parameters<T>) => {
     if (timeout) {
-      clearTimeout(timeout);
+      clearTimeout(timeout)
     }
     timeout = setTimeout(() => {
-      func(...args);
-    }, wait);
-  };
+      func(...args)
+    }, wait)
+  }
 
   debounced.cancel = () => {
     if (timeout) {
-      clearTimeout(timeout);
+      clearTimeout(timeout)
     }
-  };
+  }
 
-  return debounced as T & { cancel: () => void };
+  return debounced as T & { cancel: () => void }
+}
+
+// eslint-disable-next-line no-unused-vars
+export async function callWithDelay<T extends (...args: any[]) => any>(
+  func: T,
+  waitFor: number = 100
+): Promise<ReturnType<T>> {
+  await wait(waitFor)
+  return func()
 }
