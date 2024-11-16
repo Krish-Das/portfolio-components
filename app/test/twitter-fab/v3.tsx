304bb261197c4d1ebf1bda5d714d1df1be7bd209
Krish-Das <krishdasatgames@gmail.com>
2024-11-16 12:03:25 +0530

    Add simple barebone markup to test tha animation

--- app/test/twitter-fab/v3.tsx
+++ app/test/twitter-fab/v3.tsx
@@ -1,15 +1,32 @@
 "use client"
 
-import { IoAddSharp } from "react-icons/io5"
+import { DialogTrigger, Modal, ModalOverlay } from "react-aria-components"
+import { IoAddSharp, IoRemoveSharp } from "react-icons/io5"
 
-import { Button } from "@/app/components/buttons/ButtonV2"
+import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "@/app/components/buttons/ButtonV2"
 
 export default function TwitterFabAnimationV3() {
+  const buttonStyles = buttonVariants({ size: "iconlg" })
+
   return (
     <>
-      <Button size="iconlg" autoFocus>
-        <IoAddSharp />
-      </Button>
+      <DialogTrigger>
+        <Button size="iconlg" autoFocus>
+          <IoAddSharp />
+        </Button>
+        <ModalOverlay className="fixed inset-0 bg-red-300/10" isDismissable>
+          <Modal className="flex w-fit flex-col-reverse gap-1">
+            <button className={cn(buttonStyles)}>
+              <IoAddSharp />
+            </button>
+
+            <button className={cn(buttonStyles)}>
+              <IoRemoveSharp />
+            </button>
+          </Modal>
+        </ModalOverlay>
+      </DialogTrigger>
     </>
   )
 }
